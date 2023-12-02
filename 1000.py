11c68f82bcaaaef75e4912dab728032ec95d6790
Pablosky87 <pablosky_87@hotmail.com>
2023-12-01 19:03:41 -0500

    Mis tareas

---
+++ 1000.py
@@ -0,0 +1,4 @@
+# Su primer programa en cualquier lenguaje de programación suele ser "¡ Hola mundo! ". En este primer problema lo único que tienes que hacer es imprimir este mensaje en pantalla.
+
+print("¡ Hola mundo! ")
+
